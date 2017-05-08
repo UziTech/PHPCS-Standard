9fcc477e4f2391ee2a2a2f753ebea77df0a7fbd2
Tony Brix <Tony@Brix.ninja>
2017-05-08 11:17:58 -0500

    init commit

---
+++ UziTech/Sniffs/Debug/LogTokensSniff.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Log all tokens to error_log
+ */
+
+
+namespace UziTech\Sniffs\Debug;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class LogTokensSniff implements Sniff
+{
+
+        public $debug = false;
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_OPEN_TAG,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param File $phpcsFile The current file being checked.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        if($this->debug){
+            $tokens = $phpcsFile->getTokens();
+            ob_start();
+            var_dump($tokens);
+            $varDump = ob_get_clean();
+            error_log($varDump);
+        }
+
+    }//end process()
+
+}//end class
