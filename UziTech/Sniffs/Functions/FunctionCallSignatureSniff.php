01e8f0e65edbd7beeac1c4ddc58dccfe2071c255
Tony Brix <tony@brix.ninja>
2017-11-28 13:58:04 -0600

    use isMultiLineCall from PSR2

--- UziTech/Sniffs/Functions/FunctionCallSignatureSniff.php
+++ UziTech/Sniffs/Functions/FunctionCallSignatureSniff.php
@@ -199,8 +199,35 @@ class FunctionCallSignatureSniff implements Sniff
      */
     public function isMultiLineCall(File $phpcsFile, $stackPtr, $openBracket, $tokens)
     {
+        // If the first argument is on a new line, this is a multi-line
+        // function call, even if there is only one argument.
+        $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($openBracket + 1), null, true);
+        if ($tokens[$next]['line'] !== $tokens[$stackPtr]['line']) {
+            return true;
+        }
+
         $closeBracket = $tokens[$openBracket]['parenthesis_closer'];
-        if ($tokens[$openBracket]['line'] !== $tokens[$closeBracket]['line']) {
+
+        $end = $phpcsFile->findEndOfStatement($openBracket + 1);
+        while ($tokens[$end]['code'] === T_COMMA) {
+            // If the next bit of code is not on the same line, this is a
+            // multi-line function call.
+            $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($end + 1), $closeBracket, true);
+            if ($next === false) {
+                return false;
+            }
+
+            if ($tokens[$next]['line'] !== $tokens[$end]['line']) {
+                return true;
+            }
+
+            $end = $phpcsFile->findEndOfStatement($next);
+        }
+
+        // We've reached the last argument, so see if the next content
+        // (should be the close bracket) is also on the same line.
+        $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($end + 1), $closeBracket, true);
+        if ($next !== false && $tokens[$next]['line'] !== $tokens[$end]['line']) {
             return true;
         }
 
