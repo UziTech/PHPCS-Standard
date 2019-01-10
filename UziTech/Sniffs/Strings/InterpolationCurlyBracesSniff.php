58d5cb1a5543d9505b886fd4ff39d57411142a75
Tony Brix <tony@brix.ninja>
2019-01-09 23:15:06 -0600

    add heredoc

--- UziTech/Sniffs/Strings/InterpolationCurlyBracesSniff.php
+++ UziTech/Sniffs/Strings/InterpolationCurlyBracesSniff.php
@@ -20,7 +20,7 @@ class InterpolationCurlyBracesSniff implements Sniff {
 	 * @return array
 	 */
 	public function register() {
-		return [T_DOUBLE_QUOTED_STRING];
+		return [T_DOUBLE_QUOTED_STRING, T_HEREDOC];
 	}//end register()
 
 
@@ -36,89 +36,73 @@ class InterpolationCurlyBracesSniff implements Sniff {
 	public function process(File $phpcsFile, $stackPtr) {
 		$tokens = $phpcsFile->getTokens();
 
-		// If tabs are being converted to spaces by the tokeniser, the
-		// original content should be used instead of the converted content.
-		if (isset($tokens[$stackPtr]['orig_content']) === true) {
-			$workingString = $tokens[$stackPtr]['orig_content'];
+		$isHeredoc = $tokens[$stackPtr]['code'] === T_HEREDOC;
+
+		$workingString = "";
+		$nextToken = $stackPtr;
+		do {
+			$workingString .= isset($tokens[$nextToken]['orig_content']) ? $tokens[$nextToken]['orig_content'] : $tokens[$nextToken]['content'];
+			// $workingString .= $tokens[$nextToken]['orig_content'] ?? $tokens[$nextToken]['content'];
+			$nextToken++;
+		} while ($nextToken < $phpcsFile->numTokens && $tokens[$nextToken]['code'] === $tokens[$stackPtr]['code']);
+		$skipTo = $nextToken;
+
+		$phpString = "<?php ";
+		if ($isHeredoc) {
+			$phpString .= '"' . $workingString . '"';
 		} else {
-			$workingString = $tokens[$stackPtr]['content'];
+			$phpString .= $workingString;
 		}
 
-		$lastStringToken = $stackPtr;
-
-		$i = ($stackPtr + 1);
-		if (isset($tokens[$i]) === true) {
-			while ($i < $phpcsFile->numTokens
-				&& $tokens[$i]['code'] === $tokens[$stackPtr]['code']
-			) {
-				if (isset($tokens[$i]['orig_content']) === true) {
-					$workingString .= $tokens[$i]['orig_content'];
-				} else {
-					$workingString .= $tokens[$i]['content'];
-				}
-
-				$lastStringToken = $i;
-				$i++;
-			}
-		}
-
-		$skipTo = ($lastStringToken + 1);
-
-		// Interpolated variables should be wrapped in curly braces.
-		if ($tokens[$stackPtr]['code'] === T_DOUBLE_QUOTED_STRING) {
-			$stringTokens = token_get_all('<?php '.$workingString);
-			$error = false;
-			foreach ($stringTokens as $key => $token) {
-				if (is_array($token) === true && $token[0] === T_VARIABLE) {
-					for ($i = $key - 1; $i >= 0; $i--) {
-						$tokeni = isset($stringTokens[$i]) ? $stringTokens[$i] : null;
-						$tokeni = is_array($tokeni) ? $tokeni : [null, $tokeni, 1];
-						if ($tokeni[0] === T_OPEN_TAG || ($tokeni[0] === null && $tokeni[1] === "}")) {
-							$error = "Interpolated variables should be wrapped in curly braces";
-							break 2;
-						} else if ($tokeni[0] === T_VARIABLE || $tokeni[0] === T_CURLY_OPEN) {
-							break;
-						}
+		$stringTokens = token_get_all($phpString);
+		$error = false;
+		$errorVars = [];
+		foreach ($stringTokens as $key => $token) {
+			if (is_array($token) && $token[0] === T_VARIABLE) {
+				for ($i = $key - 1; $i >= 0; $i--) {
+					$tokeni = isset($stringTokens[$i]) ? $stringTokens[$i] : null;
+					$tokeni = is_array($tokeni) ? $tokeni : [null, $tokeni, 1];
+					if ($tokeni[0] === T_OPEN_TAG || ($tokeni[0] === null && $tokeni[1] === "}")) {
+						// $error = $workingString;
+						$error = "Interpolated variables should be wrapped in curly braces. '{$token[1]}'";
+						break 2;
+					} else if ($tokeni[0] === T_VARIABLE || $tokeni[0] === T_CURLY_OPEN) {
+						break;
 					}
 				}
 			}
+		}
 
-			if ($error) {
-				$fix = $phpcsFile->addFixableError($error, $stackPtr, 'NoCurlyVarInterpolation');
-				if ($fix === true) {
-					$newString = "";
-					while (strlen($workingString) > 0) {
-						$matches = null;
-						preg_match('/^(?:\\\\\\\\|\\\\\$|\{\$.+\}|\$[\W\d]|[^\$])+/', $workingString, $matches);
-						if (isset($matches[0]) && strlen($matches[0]) > 0) {
-							$newString .= $matches[0];
-							$workingString = substr($workingString, strlen($matches[0]));
-							continue;
-						}
-						preg_match('/^\$[^\W\d]\w*(?:\[[^\]]*\]|\([^)]*\)|->[^\W\d]\w*)*/', $workingString, $matches);
-						if (isset($matches[0]) && strlen($matches[0]) > 0) {
-							$newString .= "{{$matches[0]}}";
-							$workingString = substr($workingString, strlen($matches[0]));
-							continue;
-						}
+		if ($error) {
+			$fix = $phpcsFile->addFixableError($error, $stackPtr, 'NoCurlyVarInterpolation');
 
+			if ($fix) {
+				$newString = "";
+				while (strlen($workingString) > 0) {
+					$matches = null;
+					$isNotVar = preg_match('/^(?:\\\\.|\\{\\$.+?\\}|\\$[\\W\\d]|[^\\$])+/', $workingString, $matches);
+					$isVar = !$isNotVar && preg_match('/^\\$[^\\W\\d]\\w*(?:\\[[^\\]]*\\]|\\([^)]*\\)|->[^\\W\\d]\\w*)*/', $workingString, $matches);
+					if (isset($matches[0]) && strlen($matches[0]) > 0) {
+						$newString .= $isVar ? "{{$matches[0]}}" : $matches[0];
+						$workingString = substr($workingString, strlen($matches[0]));
+					} else {
 						// something went wrong
 						return $skipTo;
 					}
-
-
-					$phpcsFile->fixer->beginChangeset();
-					$phpcsFile->fixer->replaceToken($stackPtr, $newString);
-					while ($lastStringToken !== $stackPtr) {
-						$phpcsFile->fixer->replaceToken($lastStringToken, '');
-						$lastStringToken--;
-					}
-					$phpcsFile->fixer->endChangeset();
 				}
-			}
 
-			return $skipTo;
-		}//end if
+				$phpcsFile->fixer->beginChangeset();
+				$phpcsFile->fixer->replaceToken($stackPtr, $newString);
+
+				// <-- is the "goes to" operator. See https://stackoverflow.com/a/1642035/806777
+				while ($stackPtr <-- $nextToken) {
+					$phpcsFile->fixer->replaceToken($nextToken, '');
+				}
+				$phpcsFile->fixer->endChangeset();
+			}
+		}
+
+		return $skipTo;
 	}//end process()
 
 
