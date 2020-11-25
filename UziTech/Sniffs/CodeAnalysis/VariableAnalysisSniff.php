1ef059d3bce5f6e392424f550583da0cb5710d28
Tony Brix <tony@brix.ninja>
2020-11-25 00:06:44 -0600

    remove default undefined var names

--- UziTech/Sniffs/CodeAnalysis/VariableAnalysisSniff.php
+++ UziTech/Sniffs/CodeAnalysis/VariableAnalysisSniff.php
@@ -96,7 +96,7 @@ class VariableAnalysisSniff implements Sniff {
 	 *
 	 *  @var string|null
 	 */
-	public $validUndefinedVariableNames = "user analytics";
+	public $validUndefinedVariableNames = null;
 
 	/**
 	 * Allows unused arguments in a function definition if they are
